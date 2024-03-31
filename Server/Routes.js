1f8b9ca51ec217f5bf503d69d47eadf6386aecd3
ATHARVA279 <141947324+ATHARVA279@users.noreply.github.com>
2024-03-31 10:34:30 +0530

    getting the users

--- Server/Routes.js
+++ Server/Routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
+const User = require("./Models/userSchema");
 
 router.get("/", (req, res) => {
   res.send("SERVER WORKING!");
+});
+
+router.get("/Signup", async (req, res) => {
+  let result = await User.find({});
+  res.send(result);
 });
 
 router.get("*", (req, res) => res.status(404).send("Page not found"));
