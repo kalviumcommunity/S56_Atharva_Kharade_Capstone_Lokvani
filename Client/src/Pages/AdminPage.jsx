813eb273c4f79a0efe45de42325f4b2a69fdc596
ATHARVA279 <141947324+ATHARVA279@users.noreply.github.com>
2024-04-12 16:14:17 +0530

    done

--- Client/src/Pages/AdminPage.jsx
+++ Client/src/Pages/AdminPage.jsx
@@ -7,6 +7,7 @@ import SearchInput from '../Components/Search';
 
 const AdminPage = () => {
   const [complaints, setComplaints] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchAdminComplaints();
@@ -14,11 +15,25 @@ const AdminPage = () => {
 
   const fetchAdminComplaints = async () => {
     try {
-      const response = await axios.get('/AdminComplaints');
+      const response = await axios.get('https://s56-atharva-kharade-capstone-lokvani.onrender.com/AdminComplaints');
       setComplaints(response.data);
+      setLoading(false);
     } catch (error) {
       console.error('Error fetching admin complaints:', error);
+      setLoading(false);
     }
+  };
+
+  const renderDescription = (description) => {
+    if (description.split(' ').length > 25) {
+      return (
+        <>
+          {description.split(' ').slice(0, 25).join(' ')}...
+          <span className="view-more">View More</span>
+        </>
+      );
+    }
+    return description;
   };
 
   return (
@@ -39,40 +54,48 @@ const AdminPage = () => {
           </div>
         </div>
         <div className="Admin-Complaints">
-          {complaints.map(complaint => (
-            <div key={complaint._id} className="Admin-Complaint-box">
-              <div className="Admin-Complaint-box-left">
-                <div className="Admin-Complaint-img">
-                  <img src={complaint.Image} alt="Complaint" />
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            complaints.length > 0 ? (
+              complaints.map(complaint => (
+                <div key={complaint._id} className="Admin-Complaint-box">
+                  <div className="Admin-Complaint-box-left">
+                    <div className="Admin-Complaint-img">
+                      <img src={complaint.Image} alt="Complaint" className='complaint-img-size' />
+                    </div>
+                    <div className="Admin-Complaint-title">
+                      <h1>{complaint.title}</h1>
+                    </div>
+                    <div className="Admin-Complaint-descp">
+                      <p>{renderDescription(complaint.description)}</p>
+                    </div>
+                  </div>
+                  <div className="Admin-Complaint-box-right">
+                    <div className="Admin-Complaint-Area">
+                      <h1>Area - {complaint.area}</h1>
+                    </div>
+                    <div className="Admin-Complaint-Type">
+                      <h1>Type - {complaint.complaintType}</h1>
+                    </div>
+                    <div className="Admin-Complaint-Votes">
+                      <h1>Votes - {complaint.upvotedBy.length}</h1>
+                    </div>
+                    <div className="Admin-Complaint-Comments">
+                      <h1>Comments - 4</h1>
+                    </div>
+                    <div className="Admin-Complaint-Functions">
+                      <button>Verify</button>
+                      <button>Delete</button>
+                      <button>Re-Lodge</button>
+                    </div>
+                  </div>
                 </div>
-                <div className="Admin-Complaint-title">
-                  <h1>{complaint.title}</h1>
-                </div>
-                <div className="Admin-Complaint-descp">
-                  <p>{complaint.description}</p>
-                </div>
-              </div>
-              <div className="Admin-Complaint-box-right">
-                <div className="Admin-Complaint-Area">
-                  <h1>Area - {complaint.area}</h1>
-                </div>
-                <div className="Admin-Complaint-Type">
-                  <h1>Type - {complaint.complaintType}</h1>
-                </div>
-                <div className="Admin-Complaint-Votes">
-                  <h1>Votes - {complaint.upvotedBy.length}</h1>
-                </div>
-                <div className="Admin-Complaint-Comments">
-                  <h1>Comments - {complaint.comments.length}</h1>
-                </div>
-                <div className="Admin-Complaint-Functions">
-                  <button>Verify</button>
-                  <button>Delete</button>
-                  <button>Re-Lodge</button>
-                </div>
-              </div>
-            </div>
-          ))}
+              ))
+            ) : (
+              <p>No complaints available</p>
+            ))
+          }
         </div>
       </div>
     </div>
